1cd82e452fe15f61ef7968f53017d0a07977c4dc
AnaValentina31 <anais_casanova_iutavp2@hotmail.com>
2017-06-19 00:18:21 +0200

    Pelota del jugador
    este es el cod que te dije que dispara la pelota pero no logro hacer que rebote sino que sigue de largo, puedes probarlo en tu pc que esta no abre unity, Gracias <3

--- Assets/_Game/Scripts/Entities/Player/Playerball.cs
+++ Assets/_Game/Scripts/Entities/Player/Playerball.cs
@@ -4,13 +4,26 @@ using UnityEngine;
 
 public class Playerball : MonoBehaviour {
 
+    public float ballInitialVelocity = 600f;
+
+    private Rigibody rb;
+    private bool ballInplay;
+
 	// Use this for initialization
-	void Start () {
+	void Awake () {
+
+        rb = GetComponet<Rigidbody2D>();
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+		if (Input.GetButtonDown("Fire1")&& ballInplay == false)
+        {
+            transform.parent = null;
+            ballInplay = true;
+            rb.isKinematic = false;
+            rb.Addforce(new Vector 3(ballInitialVelocity, 0, 0));
+        }
+    }
 }
